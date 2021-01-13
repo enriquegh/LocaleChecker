c0734753ed1f9bd620614eb8d96ee136a0840878
Enrique Gonzalez <enrique@saucelabs.com>
2021-01-13 08:58:38 -0800

    adding Locale variables and exposing them

--- LocaleChecker/ContentView.swift
+++ LocaleChecker/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var languageCode: String = Locale.autoupdatingCurrent.languageCode ?? "N/A";
+    @State private var regionCode: String = Locale.autoupdatingCurrent.regionCode ?? "N/A";
+    @State private var calendarType: Calendar = Locale.autoupdatingCurrent.calendar;
+    @State private var currencyCode: String = Locale.autoupdatingCurrent.currencyCode ?? "N/A";
+    @State private var usesMetricSystem: Bool = Locale.autoupdatingCurrent.usesMetricSystem;
+    @State private var languages: [String] = Locale.preferredLanguages;
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Group {
+                Text("Language Code:")
+                Text(languageCode)
+                Text("Region Code:")
+                Text(regionCode)
+                Text("Calendar:")
+                Text(calendarType.description)
+                Text("Currency Code:")
+                Text(currencyCode)
+                Text("Uses metric?:")
+                Text(usesMetricSystem ? "Yes" : "No")
+            }
+            Text("Preferred Languages:")
+            Text(languages.joined(separator: "\n"))
+        }
+
     }
 }
 
