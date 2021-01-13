d19163989023574cc89a85551fb096e869a03f94
Enrique Gonzalez <enrique@saucelabs.com>
2021-01-13 08:23:16 -0800

    Initial Commit

---
+++ LocaleChecker/LocaleCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleCheckerApp.swift
+//  LocaleChecker
+//
+//  Created by Enrique Gonzalez on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LocaleCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
